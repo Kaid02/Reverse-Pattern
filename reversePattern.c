8d63d7d4bfee6a2c35b9f21b298422765b9353bb
Kaid02 <kabirS317@gmail.com>
2017-01-15 10:12:48 +0545

    Add files via upload

---
+++ reversePattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("\n");
+	
+		for(int i=5;i>0;i--)
+		{
+			for (int j=1;j<=5;j++)
+			{
+				if (j<i)
+				{
+						printf("  ");
+				}
+				else{
+						printf(" *");
+					}	
+		}
+		printf("\n");
+	}
+	return 0;
+	}		
